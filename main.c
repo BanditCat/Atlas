0144dbfedacd27b10d1669aba4cd22f209e17c09
BanditCat <bcj1980@gmail.com>
2024-11-21 21:49:56 -0800

    Auto-commit

--- main.c
+++ main.c
@@ -273,7 +273,9 @@ void test(){
   printStack( ts );
   tensorReverse( ts, ts->top - 2, 1 );
   tensorReverse( ts, ts->top - 1, 0 );
+  dbg( "%s\n\n", "hi" );
   printStack( ts );
+  dbg( "%s\n\n", "hi" );
   
   /* u8 d3[] = { 0 }; */
   /* u32 shape9[] = { 1 }; */
