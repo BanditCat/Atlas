8233a883a8352a642f122f7b3915165df7226aed
BanditCat <bcj1980@gmail.com>
2024-12-02 01:09:34 -0800

    Auto-commit

--- tensor.c
+++ tensor.c
@@ -425,6 +425,8 @@ tensor* newTensorInitialized( program* p, tensorStack* ts, u32 rank, u32* shape,
   }
   
   glBindBuffer( GL_ARRAY_BUFFER, compute->VBO );
+  glBindBuffer( GL_UNIFORM_BUFFER, p->ubo );
+  glUniformBlockBinding( compute->program, compute->uboLoc, 0 );
   glBindBufferBase( GL_UNIFORM_BUFFER, 0, p->ubo );
   
   // Enable the vertex attribute and set up the pointer
