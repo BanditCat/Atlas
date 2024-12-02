abd100ebe22348db3df77dfc97d118b8256b05a1
BanditCat <bcj1980@gmail.com>
2024-12-02 05:02:17 -0800

    Auto-commit

--- program.c
+++ program.c
@@ -516,7 +516,7 @@ program* newProgram( char* prog ){
     ret->varBlock = mem( offset, f32 );
     glGenBuffers( 1, &ret->ubo );
     glBindBuffer( GL_UNIFORM_BUFFER, ret->ubo );
-    glBufferData( GL_UNIFORM_BUFFER, sizeof( f32 ) * offset, NULL, GL_DYNAMIC_DRAW );
+    glBufferData( GL_UNIFORM_BUFFER, sizeof( f32 ) * offset, ret->varBlock, GL_DYNAMIC_DRAW );
     glBindBufferBase( GL_UNIFORM_BUFFER, 0, ret->ubo );
     glBindBuffer( GL_UNIFORM_BUFFER, 0 );
     //dng( "Block %s, totsize %u", glslUniformBlock, offset );
