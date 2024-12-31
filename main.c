ab0cc4f0cd1ca2da5477ab7d8b1ead2493c197ee
BanditCat <bcj1980@gmail.com>
2024-12-30 16:10:54 -0800

    Pinch to zoom

--- main.c
+++ main.c
@@ -74,9 +74,8 @@ EM_BOOL onTouch( int eventType, const EmscriptenTouchEvent *touchEvent, void *us
       float x2 = touchEvent->touches[1].canvasX;
       float y2 = touchEvent->touches[1].canvasY;
       newPinchZoom = sqrtf((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-      if( oldPinchZoom != 0.0 ){
-	exit( 0 );
 	mouseWheelDelta = 1;
+      if( oldPinchZoom != 0.0 ){
 	pinchZoom = ( newPinchZoom - oldPinchZoom );
       }
       oldPinchZoom = newPinchZoom;
