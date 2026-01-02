08f2ca242b316dfcbf803cd8ef3bcab864b7c3c2
Jon DuBois <bcj1980@gmail.com>
2026-01-02 03:27:09 -0800

    ui characters/writing

--- main.c
+++ main.c
@@ -791,8 +791,8 @@ int renderThreadFunction( void* data ){
     CHECK_GL_ERROR();
     prevTime = curTime;
     curTime = SDL_GetPerformanceCounter();
-    //    timeDelta *= 0.9; timeDelta += 0.1 * (f64)( curTime - prevTime ) / (f64)( SDL_GetPerformanceFrequency() );
-    timeDelta = (f64)( curTime - prevTime ) / (f64)( SDL_GetPerformanceFrequency() );
+    timeDelta *= 0.5; timeDelta += 0.5 * (f64)( curTime - prevTime ) / (f64)( SDL_GetPerformanceFrequency() );
+    // timeDelta = (f64)( curTime - prevTime ) / (f64)( SDL_GetPerformanceFrequency() );
       
     runTime =
       (f64)( curTime - startTime ) / (f64)( SDL_GetPerformanceFrequency() );
